656ef126f699ada12cedf2e24b23d681a6a7e7ce
Obaker815 <Obaker815@gmail.com>
2026-01-20 12:28:34 +0000

    Add project files.

---
+++ Physics Engine/Game.cs
@@ -0,0 +1,35 @@
+﻿using OpenTK.Graphics.OpenGL4;
+using OpenTK.Mathematics;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Physics_Engine
+{
+    internal class Game : GameWindow
+    {
+        public Game() 
+            : base(
+                  GameWindowSettings.Default,
+                   new NativeWindowSettings()
+                   {
+                       ClientSize = new Vector2i(800, 600),
+                       Title = "OpenGL in C#"
+                   })
+        {
+        }
+
+        protected override void OnLoad()
+        {
+            GL.ClearColor(0.1f, 0.1f, 0.1f, 1f);
+        }
+
+        protected override void OnRenderFrame(FrameEventArgs e)
+        {
+            GL.Clear(ClearBufferMask.ColorBufferBit);
+            SwapBuffers();
+        }
+    }
+}
---
+++ Physics Engine/Program.cs
@@ -0,0 +1,12 @@
+namespace Physics_Engine
+{
+    internal static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using var game = new Game();
+            game.Run();
+        }
+    }
+}
